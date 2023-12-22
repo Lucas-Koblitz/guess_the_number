0edf48ef3b49f63d81fd1a7ead9130fc0486e259
lucaskoblitz <lcalandino@gmail.com>
2023-12-22 18:22:39 -0300

    pc guess added

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import random 
 
-def guess(x):
+def userGuess(x):
     number = random.randint(1, x)
     guess = 0
     while guess != number:
         guess = int(input("Type a number: "))
         if guess < number:
             print("too bad, the number is HIGHER, guess again ")
-        elif guess >number:
+        elif guess > number:
             print("too bad, the number is LOWER, guess again ")
+            
+def pcGuess(x):
+    low = 1
+    high = x
+    response = ''
+    while response != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        response = input(f"if your number is higher than  {guess}  press (h), if it's lower, press (l), if it's correct, press (c) ")
+        if response == 'l':
+            high = guess-1
+        elif response == 'h':
+            low = guess+1 
         
-guess(19)
+            
+    print(f"the number you imagined is {guess}")
+
 print("Congrats, you guessed it right!")
+userGuess(123)
