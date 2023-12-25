0c520e32adff417d59f05dc181fc72110f583fd6
lucaskoblitz <lcalandino@gmail.com>
2023-12-25 18:10:06 -0300

    added user choice functionality to choose between pc guess and user guess

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import random 
 
+def choice():
+    
+    usr_choice = input("If you want the computer to discover you number type 'pc' (number needs to be in a range between 1 and 10 000). If you want to discover the computer number, type 'me' (the number is between 1 and 500)")
+    if usr_choice == 'pc':
+        pcGuess(10000) 
+    elif usr_choice == 'me':
+        userGuess(500)
+    else:
+        choice()
+
+
 def userGuess(x):
     number = random.randint(1, x)
     guess = 0
     while guess != number:
-        guess = int(input("Type a number: "))
-        if guess < number:
-            print("too bad, the number is HIGHER, guess again ")
-        elif guess > number:
-            print("too bad, the number is LOWER, guess again ")
+        try:
+            guess = int(input("Type a number: "))
+            if guess < number:
+                print("too bad, the number is HIGHER, guess again ")
+            elif guess > number:
+                print("too bad, the number is LOWER, guess again ")
+        except ValueError:
+            print("That's not a valid number, it can't be a decimal or contain letters, guess again ")
+    print(f"Congrats, you guessed it right!, the number was {guess}")
             
 def pcGuess(x):
     low = 1
@@ -28,5 +43,4 @@ def pcGuess(x):
             
     print(f"the number you imagined is {guess}")
 
-print("Congrats, you guessed it right!")
-userGuess(123)
+choice()
